ccf7f60903bc3a13f483a8b6be5e2e0e4656e04a
watik <apiwatik@gmail.com>
2021-03-01 10:33:01 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2-workshop-items-alias',
+    //'dsn' => 'mysql:host=127.0.0.1;dbname=yii2-workshop-items-alias',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=test',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
